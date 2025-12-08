e409ec44e62b50be46506a748737c80cbc8ad3f9
Alex Synelnikov <alex.synelnikov@gmail.com>
2025-12-08 23:16:17 +0200

    GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alexsin0.github.io",
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()],
   },
